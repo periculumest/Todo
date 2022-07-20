87bb2cfd59f5db2fbe2b2618db62d123dea1615e
Jesse Price <periculumest@gmail.com>
2022-07-19 21:20:21 -0700

    init

---
+++ TodoAPI.DataLayer/Repositories/ListRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoAPI.DataLayer.Repositories
+{
+    internal class ListRepository
+    {
+    }
+}
---
+++ TodoAPI/Controllers/TodoController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoAPI.Controllers
+{
+    public class TodoController : Controller
+    {
+       
+    }
+}
